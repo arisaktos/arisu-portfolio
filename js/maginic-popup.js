60e64b21fc19be6dfa463ed4eb3703dadb0e3d38
Alicja <arisusaktos@gmail.com>
2017-11-01 14:54:02 +0100

    sdf

--- js/maginic-popup.js
+++ js/maginic-popup.js
@@ -1,1 +1,10 @@
-$("#section-popups").magnificPopup({delegate:"a",removalDelay:500,callbacks:{beforeOpen:function(){this.st.mainClass=this.st.el.attr("data-effect")}},midClick:!0});
+$("#section-popups").magnificPopup({
+    delegate: "a",
+    removalDelay: 500,
+    callbacks: {
+        beforeOpen: function () {
+            this.st.mainClass = this.st.el.attr("data-effect")
+        }
+    },
+    midClick: !0
+});
